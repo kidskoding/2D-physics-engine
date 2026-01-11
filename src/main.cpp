77e70a976811a3cc5bf1575a1fba8ed8e74f96e8
Anirudh Konidala <kidskodingclub@gmail.com>
2026-01-10 20:54:13 -0600

    updated to cloud (github)

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,14 @@
 #include "CircleCollider2D.h"
 #include "PhysicsObject.h"
 #include "Rigidbody2D.h"
-#include "SFML/Window/Event.hpp"
-
-#include <SFML/Window.hpp>
-#include <SFML/Graphics.hpp>
-#include <algorithm>
 
 int main() {
-    auto window = sf::RenderWindow(sf::VideoMode({800, 600}), "2D-Physics Engine Simulator");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "2D Physics Engine Demo");
     window.setFramerateLimit(60);
 
     auto* ballShape = new sf::CircleShape(25.0f);
     ballShape->setFillColor(sf::Color::Green);
-    ballShape->setOrigin(sf::Vector2f(25.0f, 25.0f));
+    ballShape->setOrigin(25.0f, 25.0f);
 
     Rigidbody2D ballRb(100.0f, 100.0f, 1.0f);
     auto* ballCollider = new CircleCollider2D(25.0f, Vector2D(100.0f, 100.0f));
@@ -23,42 +18,33 @@ int main() {
 
     auto* boxShape = new sf::RectangleShape(sf::Vector2f(100.0f, 100.0f));
     boxShape->setFillColor(sf::Color::Blue);
-    boxShape->setOrigin(sf::Vector2f(50.0f, 50.0f));
+    boxShape->setOrigin(50.0f, 50.0f);
 
     Rigidbody2D boxRb(400.0f, 300.0f, 0.0f);
     auto* boxCollider = new BoxCollider2D(100.0f, 100.0f, Vector2D(400.0f, 300.0f));
     PhysicsObject box(boxRb, boxCollider, boxShape);
 
-    bool isColliding = false;
     sf::Clock clock;
 
     while(window.isOpen()) {
         sf::Event event;
         while(window.pollEvent(event)) {
-            if(event.type == sf::Event::Closed) {
+            if(event.type == sf::Event::Closed)
                 window.close();
-            }
         }
 
-        float deltaTime = clock.restart().asSeconds();
-
-        deltaTime = std::min(deltaTime, 0.1f);
+        float deltaTime = std::min(clock.restart().asSeconds(), 0.1f);
 
         ball.update(deltaTime);
         box.update(deltaTime);
 
-        isColliding = ball.collider->checkCollision(*box.collider);
-
-        if(isColliding) {
+        if(ball.collider->checkCollision(*box.collider)) {
             ballShape->setFillColor(sf::Color::Red);
             boxShape->setFillColor(sf::Color::Red);
 
-            Vector2D ballPos = ball.rigidbody.getPosition();
-            Vector2D boxPos = box.rigidbody.getPosition();
-            Vector2D normal = (ballPos - boxPos).normalize();
-
-            ball.rigidbody.setPosition(ballPos + normal * 2.0f);
-            ball.rigidbody.setVelocity(normal * ball.rigidbody.getVelocity().magnitude() * -0.8f);
+            Vector2D normal = (ball.rigidbody.getPosition() - box.rigidbody.getPosition()).normalize();
+            ball.rigidbody.setPosition(ball.rigidbody.getPosition() + normal * 2.0f);
+            ball.rigidbody.setVelocity(normal * -0.8f * ball.rigidbody.getVelocity().magnitude());
         } else {
             ballShape->setFillColor(sf::Color::Green);
             boxShape->setFillColor(sf::Color::Blue);
@@ -70,18 +56,12 @@ int main() {
         if(ballPos.x < 0 || ballPos.x > 800) {
             Vector2D vel = ball.rigidbody.getVelocity();
             ball.rigidbody.setVelocity(Vector2D(-vel.x * 0.8f, vel.y));
-            ball.rigidbody.setPosition(Vector2D(
-                ballPos.x < 0 ? 5 : 795,
-                ballPos.y
-            ));
+            ball.rigidbody.setPosition(Vector2D(ballPos.x < 0 ? 5 : 795, ballPos.y));
         }
         if(ballPos.y < 0 || ballPos.y > 600) {
             Vector2D vel = ball.rigidbody.getVelocity();
             ball.rigidbody.setVelocity(Vector2D(vel.x, -vel.y * 0.8f));
-            ball.rigidbody.setPosition(Vector2D(
-                ballPos.x,
-                ballPos.y < 0 ? 5 : 595
-            ));
+            ball.rigidbody.setPosition(Vector2D(ballPos.x, ballPos.y < 0 ? 5 : 595));
         }
 
         window.clear(sf::Color::Black);
